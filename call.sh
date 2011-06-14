2aaa3d63c8e032951a2239122f1942c81857eb3a
Jeremy Stober <stober@gmail.com>
2011-06-14 11:19:55 +0200

    Can read turtlesim/Pose messages out of a bagfile. Moving to make bagfile format generic. Will need to code robot specific bagfile writers.

--- call.sh
+++ call.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rosservice call seds_server ["/home/stober/workspace/ros/seds/data/test.ml"] 0 "model.txt"
+#rosservice call seds_server ["/home/stober/workspace/ros/seds/data/test.ml"] 0 "model.txt"
+rosservice call seds_server ["/home/stober/workspace/ros/seds/turtle/bag0.bag"] 1 "model.txt"
--- src/seds_node.cpp
+++ src/seds_node.cpp
@@ -1,9 +1,15 @@
 #include "ros/ros.h"
 #include "std_srvs/Empty.h"
 
+#include "boost/foreach.hpp"
+#include "rosbag/bag.h"
+#include "rosbag/view.h"
+
 #include "seds/SedsOptimize.h"
 #include "mldemos/dynamicalSEDS.h"
 #include "process_mlfile.hpp"
+#include "turtlesim/Pose.h"
+
 
 bool sedsSRV(seds::SedsOptimize::Request  &req, seds::SedsOptimize::Response &res )
 {
@@ -12,10 +18,13 @@ bool sedsSRV(seds::SedsOptimize::Request  &req, seds::SedsOptimize::Response &re
   float dT;
   ivec labels;
 
-  if (req.filetype == 0){
+  ROS_INFO("Called on filetype: %d", req.filetype);
+
+  if (req.filetype == 0){ // an ml file generated using mldemos
+
     // process the input file into trajectories for training
     string filename = req.filenames[0];
-    process_dataset_manager_file(filename, trajectories, labels, dT); 
+    process_dataset_manager_file(filename, trajectories, labels, dT);
     seds.dT = dT;
 
     // train the model
@@ -23,7 +32,34 @@ bool sedsSRV(seds::SedsOptimize::Request  &req, seds::SedsOptimize::Response &re
 
     // save the model
     seds.seds->saveModel(req.outputfile.c_str());
-  } else {
+  } else if (req.filetype == 1){ // a list of bag files
+
+    ROS_INFO("Filesize: %d", req.filenames.size());
+
+    // # of bagfiles -- every bag file contains one trajectory
+    trajectories.resize(req.filenames.size());
+
+    for (int i=0;i<req.filenames.size();i++){
+
+      // initialize the bag view
+      rosbag::Bag bag;
+      // vector<string> topics;
+      // topics.push_back("turtle1/pose"); // TODO - make this a generic parameter
+      bag.open(req.filenames[i], rosbag::bagmode::Read);
+      rosbag::View view(bag, rosbag::TopicQuery("turtle1/pose"));
+
+      ROS_INFO("Processing bag: %s", req.filenames[i].c_str());
+      BOOST_FOREACH(rosbag::MessageInstance const m, view){
+      	turtlesim::Pose::ConstPtr p = m.instantiate<turtlesim::Pose>();
+	ros::Time t = m.getTime();
+	ROS_INFO("t: %f", t.toSec());
+      	ROS_INFO("x: %f", p->x);
+      }
+
+
+    }
+  }
+  else {
     ROS_ERROR("Filetype not supported!");
   }
 

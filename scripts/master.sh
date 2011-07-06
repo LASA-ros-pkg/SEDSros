300c242f14fad9865c68889b88e5c18d0b732fbe
Jeremy Stober <stober@gmail.com>
2011-07-06 17:22:08 +0200

    Modified the master.sh script to be more sensible.

--- scripts/master.sh
+++ scripts/master.sh
@@ -23,11 +23,10 @@ function record_bag {
     cd -
 }
 
-function record_bagfiles {
+function run_demo {
     ndemo=1
-    OPTIONS="Record Learn Quit"
-    LEARN=0
-    while [ $LEARN = 0 ]; do
+    OPTIONS="Record Learn Run Quit"
+    while [ 1 ]; do
 
       #clear
       echo "Please select one of the following options."
@@ -39,8 +38,42 @@ function record_bagfiles {
 	  record_bag $ddir $ndemo
 	  let ndemo=ndemo+1
 	elif [ "$opt" = "Learn" ]; then
-	  LEARN=1
+
+	    echo "Beginning the training process!"
+	    echo "Processing the demonstration bagfiles..."
+
+	    rosrun seds tf2seds.py -b $ddir -o /tmp/tmp.bag
+
+	    echo "Learning new model parameters using seds!"
+
+            # optimization
+	    rosservice call /seds/optimize /tmp/tmp.bag
+
+            # load model parameters into ds_node
+	    rosservice call /ds_node/load_model
+
+	    echo "Model is learned and loaded."
 	  break
+	elif [ "$opt" = "Run" ]; then
+
+	    echo "Press enter to start the r_cart controller."
+
+	    read
+	    rosrun pr2_controller_manager pr2_controller_manager start r_cart
+
+	    echo "Press enter to begin to drive the robot using the model."
+
+	    read
+	    rosservice call /pr2_driver/start
+
+	    echo "Press enter again to stop the robot."
+
+	    read
+	    rosservice call /pr2_driver/stop
+
+	    echo "Stopping the r_cart controller."
+	    rosrun pr2_controller_manager pr2_controller_manager stop r_cart
+
 	else
 	  #clear
 	  echo "Bad option!"
@@ -57,41 +90,12 @@ mkdir -p $ddir
 # This starts up the low-level topic-based cartesian control system (but turns off all pr2 controllers).
 # launch_silent seds jtteleop.launch &
 
-# This starts up seds and ds nodes which will be used for learning and controlling the robot.
+# This starts up seds and ds and pr2_driver nodes which will be used for learning and controlling the robot.
 launch_silent seds seds.launch &
 
 # Stop the controller for the moment while we record bagfiles.
 # rosrun pr2_controller_manager pr2_controller_manager stop r_cart
 
-# Record example bagfiles.
-record_bagfiles
+# Run the demo.
+run_demo
 
-echo "Beginning the training process!"
-echo "Processing the bagfiles..."
-
-rosrun seds tf2seds.py -b $ddir -o $ddir/tmp.bag
-
-echo "Learning new model parameters using seds!"
-
-# optimization
-rosservice call /seds/optimize $ddir/tmp.bag
-
-# load model parameters into ds_node
-rosservice call /ds_node/load_model
-
-echo "Model is learned and loaded."
-
-echo "Press enter to start the r_cart controller."
-
-read
-rosrun pr2_controller_manager pr2_controller_manager start r_cart
-
-echo "Press enter to begin to drive the robot using the model."
-
-read
-rosservice call /pr2_driver/start
-
-echo "Press enter again to stop the robot."
-
-read
-rosservice call /pr2_driver/stop

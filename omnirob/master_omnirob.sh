b8eaab85767813eaf41dfaf54d3bc543c9821bd9
Christophe Paccolat <christophe.paccolat@epfl.ch>
2011-11-06 23:15:48 +0100

    Temp commit for master script, bugfix in progress...

--- omnirob/master_omnirob.sh
+++ omnirob/master_omnirob.sh
@@ -6,10 +6,11 @@ function launch_silent {
     local NODE=$1
     local FILE=$2
     local OPTS=$3
-    if [ -z OPTS ]; then
-	roslaunch $NODE $FILE >> /tmp/seds.out 2>> /tmp/seds.err &
+    if [ -z $OPTS ]; then
+		roslaunch $NODE $FILE >>/tmp/seds.out 2>>/tmp/seds.err &
     else
-	roslaunch $NODE $FILE $OPTS >> /tmp/seds.out 2>> /tmp/seds.err &
+		roslaunch $NODE $FILE $OPTS >>/tmp/seds.out &
+#	roslaunch $NODE $FILE $OPTS >>/tmp/seds.out 2>>/tmp/seds.err &
     fi
 
     local PID=$!
@@ -29,7 +30,7 @@ function record_bag {
     read
     rosbag record /joints_fb &
     ID=$!
- 
+
     read
     kill -2 $ID # send a SIGINT signal to kill the recording process
 
@@ -41,75 +42,95 @@ function run_demo {
     ndemo=1
     OPTIONS="Record Learn Run Quit"
     while [ 1 ]; do
-	echo "Please select one of the following options."
-	select opt in $OPTIONS; do
-	    if [ "$opt" = "Quit" ]; then
-	  echo "Quiting..."
-	  exit
-	elif [ "$opt" = "Record" ]; then
-	    OMNIPID=$(launch_silent orca_proxy orca_base.launch serverip:=128.178.145.171)
-	    #rosservice call /omnirob/idle
-	    record_bag $ddir $ndemo
-	    let ndemo=ndemo+1
-	elif [ "$opt" = "Learn" ]; then
+		echo "Please select one of the following options."
+		opt="ss"
+        select opt in $OPTIONS; do
+	    	if [ "$opt" = "Quit" ]; then
+	  			echo "Quiting..."
+	  			exit
+			elif [ "$opt" = "Record" ]; then
+	    		OMNIPID=$(launch_silent orca_proxy orca_base.launch simulatorip:=128.178.145.171)
+				#rosservice call /omnirob/idle
+	    		record_bag $ddir $ndemo
+	    		let ndemo=ndemo+1
+			elif [ "$opt" = "Learn" ]; then
+				if [ -z $ddir ]; then
+					echo "Gimme the bags ! Where are they ?..."
+					exit
+	    		fi
 
-	    echo "Beginning the training process!"
-	    echo "Processing the demonstration bagfiles..."
+				echo "Beginning the training process!"
+	    		echo "Processing the demonstration bagfiles..."
+	    		rosrun seds omni2seds.py -b $ddir -o /tmp/tmp.bag
+				if [ $? -ne 0 ]; then
+					echo "Error while running seds wrapper script"
+					exit
+				if
 
-	    rosrun seds omni2seds.py -b $ddir -o /tmp/tmp.bag ||  ( echo "Error while running seds wrapper script" && exit )
+				# optimization
+	    		echo "Learning new model parameters using seds!"
+	    		rosservice call /seds/optimize /tmp/tmp.bag
+	    		if [ $? -ne 0 ]; then
+					echo "Error while running SEDS optimization"
+					exit
+				fi
 
-	    echo "Learning new model parameters using seds!"
+	    		# in case you need to restart
+	    		rosservice call /seds/save_file $(pwd)/$ddir/model.bag
+	    		if [ $? -ne 0 ]; then
+					echo "Error while saving model to file"
+					exit
+				fi
 
-            # optimization
-	    rosservice call /seds/optimize /tmp/tmp.bag ||  ( echo "Error while running SEDS optimization" && exit )
+	    		echo "Model is learned and loaded."			elif [ "$opt" = "Run" ]; then
+				echo "Running the orca driver"
+	  			# Issues when running orca like this...
+	  			#OMNIPID=$(launch_silent orca_proxy orca_base.launch simulatorip:=128.178.145.171)
+	  			#	    echo "pid=$OMNIPID"
+	  			# load model parameters into ds_node
+	  			DS_STATE=$(rosservice call /ds_node/is_loaded 2>&1)
+	  			DS_LOADED="loaded: True"
+	  			if [ "$DS_STATE" != "$DS_LOADED" ]; then
+	  				if [ -e /tmp/model.bag ]; then
+						echo "Loading model from saved file"
+						rosservice call /ds_node/load_file $(pwd)/$ddir/model.bag
+						if [ $? -ne 0 ]; then
+							echo "Error while loading from file"
+							exit
+						fi
+	  				else
+						echo "Loading model from cache:"
+						rosservice call /ds_node/load_model
+						if [ $? -ne 0 ]; then
+							echo "Error while loading from cache"
+							exit
+						fi
+	  				fi
+	  			fi
 
-	    # in case you need to restart
-	    rosservice call /seds/save_file /tmp/model.bag ||  ( echo "Error while saving model to file" && exit )
+	  			DS_STATE=$(rosservice call /ds_node/is_loaded 2>&1)
+	  			if [[ $DS_STATE == *ERROR* ]]; then
+	  				echo "Could not load any model, did you learn one ?"
+					exit
+	  			fi
 
-	    echo "Model is learned and loaded."
-	  break
-	elif [ "$opt" = "Run" ]; then
+	  			echo "Move the Omnirob to a test position and press enter!"
+	  			read
 
-	  echo "Running the orca driver"
-	  OMNIPID=$(launch_silent orca_proxy orca_base.launch serverip:=128.178.145.171)
-	  
-	  # load model parameters into ds_node
-	  DS_STATE=$(rosservice call /ds_node/is_loaded 2>&1)
-	  DS_LOADED="loaded: True"
-	  if [ "$DS_STATE" != "$DS_LOADED" ]; then
-	  	if [ -e /tmp/model.bag ]; then
-			echo "Loading model from saved file"
-			rosservice call /ds_node/load_file /tmp/model.bag ||  ( echo "Error while loading from file" && exit )
-	  	else
-			echo "Loading model from cache:"
-			rosservice call /ds_node/load_model || ( echo "Error while loading from cache" && exit )
-	  	fi
-	  fi
+	  			echo "Activating Omnirob and calling Omnirob driver!"
+	  			rosservice call /omnirob_driver/start # starts the driver service
 
-	  DS_STATE=$(rosservice call /ds_node/is_loaded 2>&1)
-	  if [[ $DS_STATE == *ERROR* ]]; then
-	  	echo "Could not load any model, did you learn one ?"
-		exit
-	  fi
+	  			echo "Press enter to stop driving the robot."
+	  			read
+	  			rosservice call /omnirob_driver/stop
+	  			#kill -2 $OMNIPID  # send a SIGINT signal to kill orca_proxy
+	  			echo "Done"
 
-	  echo "Move the Omnirob to a test position and press enter!"
-	  read
-
-	  echo "Activating Omnirob and calling Omnirob driver!"
-	  rosservice call /omnirob_driver/start # starts the driver service
-
-	  echo "Press enter to stop driving the robot."
-	  read
-	  rosservice call /omnirob_driver/stop
-	  kill -2 $OMNIPID  # send a SIGINT signal to kill orca_proxy
-	  echo "Done"
-
-	else
-	  #clear
-	  echo "Bad option!"
-	fi
-      done
-    done
+			else
+	  			echo "Bad option!"
+			fi
+		done
+	done
 }
 
 # The script requires a working directory.
